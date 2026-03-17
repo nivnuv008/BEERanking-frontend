a2b42eb4e27f53d93aaec9b90ae01f7b39910ede
Niv Cohen <nivnuv008@gmail.com>
2026-03-17 19:21:55 +0200

    delete things to ignore

--- .vite/deps/react-bootstrap_ButtonGroup.js
+++
@@ -1,9 +0,0 @@
-"use client";
-import {
-  ButtonGroup_default
-} from "./chunk-WQYZ77RE.js";
-import "./chunk-CQC52RUO.js";
-import "./chunk-G4V7ITBN.js";
-export {
-  ButtonGroup_default as default
-};
--- .vite/deps/react-bootstrap_Spinner.js
+++
@@ -1,9 +0,0 @@
-"use client";
-import {
-  Spinner_default
-} from "./chunk-AJSJSXCW.js";
-import "./chunk-CQC52RUO.js";
-import "./chunk-G4V7ITBN.js";
-export {
-  Spinner_default as default
-};
